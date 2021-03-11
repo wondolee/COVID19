ed1c8df852b48ac6ea4426d5a990f0d7d0bd9fd0
Won Do Lee <61145109+wondolee@users.noreply.github.com>
2021-03-11 22:45:29 +0900

    Update

--- code/Model_EST.R
+++ code/Model_EST.R
@@ -8,9 +8,9 @@ require(rgdal)
 
 ###Importing the input data 
 ccg.spoly<-readRDS("ccg.spoly.geojson")
-ccg.spoly$H_06<-ccg.spoly$H_04+ccg.spoly$H_05 ##Updating the share of population in bad health; H_04 (bad health) and H_05 (very bad health)
+ccg.spoly$H_06<-ccg.spoly$H_04+ccg.spoly$H_05 ##Updating the variable of share of population in bad health; H_04 (bad health) and H_05 (very bad health)
 
-###OLS model 
+###Building the OLS models: d1;24 March, d2;31 March, d3; 7 April, and d4; 14 April 2020.
 step.model.d1<-lm(formula = scale(PER1) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06), data = ccg.spoly)
 step.model.d2<-lm(formula = scale(PER2) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06), data = ccg.spoly)
 step.model.d3<-lm(formula = scale(PER3) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06), data = ccg.spoly)
@@ -60,47 +60,49 @@ summary(lag.model.d1,Nagelkerke=T) ##retaining the pseudo R2 for spatial autoreg
 ###GW model
 require(GWmodel)
 dist<-gw.dist(coordinates(as(ccg.spoly,"Spatial"))) ##Calculate a distance vector for GW model calibration points
+
+##Model specifications
 reg1.mod = as.formula(scale(PER1) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06))
 reg2.mod = as.formula(scale(PER2) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06))
 reg3.mod = as.formula(scale(PER3) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06))
 reg4.mod = as.formula(scale(PER4) ~ scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06))
 
-##Retaining the bandwidth for GWR model
-gwr.bwd.f1 <- bw.gwr(reg1.mod, ccg.spoly, approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
-gwr.bwd.f2 <- bw.gwr(reg1.mod, ccg.spoly, approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
-gwr.bwd.f3 <- bw.gwr(reg1.mod, ccg.spoly, approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
-gwr.bwd.f4 <- bw.gwr(reg1.mod, ccg.spoly, approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
+##Retaining the bandwidths for GWR models - would be get 155 neighbourhoods in consecutive 4 Tuesdays (24 March, 31 March, 7 April, and 14 April)
+gwr.bwd.d1 <- bw.gwr(reg1.mod, as(ccg.spoly,"Spatial"), approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
+gwr.bwd.d2 <- bw.gwr(reg2.mod, as(ccg.spoly,"Spatial"), approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
+gwr.bwd.d3 <- bw.gwr(reg3.mod, as(ccg.spoly,"Spatial"), approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
+gwr.bwd.d4 <- bw.gwr(reg4.mod, as(ccg.spoly,"Spatial"), approach="AICc", kernel="bisquare", adaptive=TRUE, dMat=dist)
 
 ##Building the GWR model - using Adaptive bi-square kernel
-mod.GWR.f1 <- gwr.basic(reg1.mod, data = ccg.spoly, bw = gwr.bwd.f1, kernel="bisquare", adaptive=TRUE, dMat=dist)
-mod.GWR.f2 <- gwr.basic(reg2.mod, data = ccg.spoly, bw = gwr.bwd.f2, kernel="bisquare", adaptive=TRUE, dMat=dist)
-mod.GWR.f3 <- gwr.basic(reg3.mod, data = ccg.spoly, bw = gwr.bwd.f3, kernel="bisquare", adaptive=TRUE, dMat=dist)
-mod.GWR.f4 <- gwr.basic(reg4.mod, data = ccg.spoly, bw = gwr.bwd.f4, kernel="bisquare", adaptive=TRUE, dMat=dist)
+mod.GWR.d1 <- gwr.basic(reg1.mod, data = as(ccg.spoly,"Spatial"), bw = gwr.bwd.f1, kernel="bisquare", adaptive=TRUE, dMat=dist)
+mod.GWR.d2 <- gwr.basic(reg2.mod, data = as(ccg.spoly,"Spatial"), bw = gwr.bwd.f2, kernel="bisquare", adaptive=TRUE, dMat=dist)
+mod.GWR.d3 <- gwr.basic(reg3.mod, data = as(ccg.spoly,"Spatial"), bw = gwr.bwd.f3, kernel="bisquare", adaptive=TRUE, dMat=dist)
+mod.GWR.d4 <- gwr.basic(reg4.mod, data = as(ccg.spoly,"Spatial"), bw = gwr.bwd.f4, kernel="bisquare", adaptive=TRUE, dMat=dist)
 
 ##Exporting the outcomes of GWR model estimation results into SpatialPolygonsDataFrame
-new.gwr.m3.d1<-mod.GWR.f1$SDF
-new.gwr.m3.d2<-mod.GWR.f2$SDF
-new.gwr.m3.d3<-mod.GWR.f3$SDF
-new.gwr.m3.d4<-mod.GWR.f4$SDF
+new.gwr.m3.d1<-mod.GWR.d1$SDF
+new.gwr.m3.d2<-mod.GWR.d2$SDF
+new.gwr.m3.d3<-mod.GWR.d3$SDF
+new.gwr.m3.d4<-mod.GWR.d4$SDF
 
-##Building the MGWR model; bandwidth calibration starting from GWR bandwidth (i.e. 155 in this analysis)
-mod.MGWR.f1 <- gwr.multiscale(reg1.mod,as(ccg.spoly,"Spatial"),criterion="dCVR", kernel = "bisquare", adaptive=T,  
+##Building the MGWR model; bandwidth calibration starting from GWR bandwidth (BW=155)
+mod.MGWR.d1 <- gwr.multiscale(reg1.mod,as(ccg.spoly,"Spatial"),criterion="dCVR", kernel = "bisquare", adaptive=T,  
                               bws0=c(155,155,155,155,155,155,155),dMats=list(dist,dist,dist,dist,dist,dist,dist),
                               verbose = T, hatmatrix = T)
-mod.MGWR.f2 <- gwr.multiscale(reg2.mod,as(ccg.spoly,"Spatial"), criterion="dCVR", kernel = "bisquare", adaptive=T,  
+mod.MGWR.d2 <- gwr.multiscale(reg2.mod,as(ccg.spoly,"Spatial"), criterion="dCVR", kernel = "bisquare", adaptive=T,  
                               bws0=c(155,155,155,155,155,155,155),dMats=list(dist,dist,dist,dist,dist,dist,dist),
                               verbose = T, hatmatrix = T)
-mod.MGWR.f3 <- gwr.multiscale(scale(PER3)~scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06)
+mod.MGWR.d3 <- gwr.multiscale(scale(PER3)~scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06)
                               ,as(ccg.spoly,"Spatial"), criterion="dCVR", kernel = "bisquare", adaptive=T,  
                               bws0=c(155,155,155,155,155,155,155),dMats=list(dist,dist,dist,dist,dist,dist,dist),
                               verbose = T, hatmatrix = T)
-mod.MGWR.f4 <- gwr.multiscale(scale(PER4)~scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06)
+mod.MGWR.d4 <- gwr.multiscale(scale(PER4)~scale(NOT_SPE) + scale(POP_DEN)+ scale(INC_80) + scale(slf_mpl)+scale(SG_C1)+scale(H_06)
                               ,as(ccg.spoly,"Spatial"), criterion="dCVR", kernel = "bisquare", adaptive=T,  
                               bws0=c(155,155,155,155,155,155,155),dMats=list(dist,dist,dist,dist,dist,dist,dist),
                               verbose = T, hatmatrix = T)
 							  
 ##Exporting the outcomes of MGWR model estimation results into SpatialPolygonsDataFrame
-new.MGWR.m3.d1<-mod.GWR.f1$SDF
-new.MGWR.m3.d2<-mod.GWR.f2$SDF
-new.MGWR.m3.d3<-mod.GWR.f3$SDF
-new.MGWR.m3.d4<-mod.GWR.f4$SDF
+new.MGWR.m3.d1<-mod.GWR.d1$SDF
+new.MGWR.m3.d2<-mod.GWR.d2$SDF
+new.MGWR.m3.d3<-mod.GWR.d3$SDF
+new.MGWR.m3.d4<-mod.GWR.d4$SDF
